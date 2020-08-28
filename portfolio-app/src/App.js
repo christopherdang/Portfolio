2060a7f4f26109d9cab3b8579ec4d2c9d993bb53
Christopher Dang <officialchristopherdang@gmail.com>
2020-08-28 01:12:29 -0400

    Created routing

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 import './App.css';
 
 class App extends React.Component {
@@ -39,9 +41,11 @@ class App extends React.Component {
   render() {
 
     return (
-      <div>
-        Hello from React
-      </div>
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <p>Hi from react</p>
+        </Container>
+      </Router>
       );
   }
 }
