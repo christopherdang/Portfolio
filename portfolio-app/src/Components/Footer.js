b875b09cec7ecf95c883dadd987d80a35eef0b5a
Christopher Dang <officialchristopherdang@gmail.com>
2020-09-12 09:23:17 -0400

    Finished footer

--- portfolio-app/src/Components/Footer.js
+++ portfolio-app/src/Components/Footer.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Footer() {
 
     return(
-        <footer className='mt=5'>
-            <Container>
-                <Row>
+        <footer className="mt-5">
+            <Container fluid={true}>
+                <Row className="border-top justify-content-between p-3">
                     <Col className="p-0" md={3} sm={12}>
                         Christopher Dang
+                    </Col>
+                    <Col className="p-0 d-flex justify-content-end" md={2.1}>
+                        This website was created by Christopher Dang
                     </Col>
                 </Row>
             </Container>
