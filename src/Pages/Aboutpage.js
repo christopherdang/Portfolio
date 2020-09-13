a6e19a1aca4573aea5a3e51969e4c88634c7f2f7
Christopher Dang <officialchristopherdang@gmail.com>
2020-09-12 22:34:33 -0400

    Updated about page

--- src/Pages/Aboutpage.js
+++ src/Pages/Aboutpage.js
@@ -11,8 +11,17 @@ function Aboutpage(props) {
 
             <Content>
                 <p><font color="white">My name is Christopher Dang and I am currently majoring in Computer Science at the University of Florida</font></p>
+                
+                <p><font color="white">
+                    I was born in Tampa, Florida and grew up with a loving and supportive family. 
+                </font></p>
 
-                <p><font color="white">I have a passion for computer programming, whether it is back or front-end development.</font></p>
+                <p><font color="white">
+                    I have a passion for computer programming, whether it is back or front-end development.
+                    I enjoy every aspect there is to coding and strive to become a more diverse programmer. 
+                    For example, on recent projects I have worked on, I was involved in both back-end and front-end aspects. 
+                    Additionally, most of the work I've done is due to researching online and learning how to use programs on my own.
+                </font></p>
             </Content>
         </dive>
     );
